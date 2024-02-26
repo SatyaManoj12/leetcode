5e2f2add2a458066c392e4982955338322297430
SatyaManoj12 <119724289+SatyaManoj12@users.noreply.github.com>
2024-02-26 21:37:52 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (48.09%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -9,15 +9,12 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==NULL and q==NULL) return true;
-        if(p==NULL or q==NULL) return false;
-        if(p->val!=q->val) return 0;
-      //  if(p->val==q->val)
-        return    isSameTree(p->left,q->left) and isSameTree(p->right,q->right);
-        return 0;
+        if(p==NULL and q==NULL) return true ;
+            if(p==NULL or q==NULL) return false;
+                if(p->val!=q->val) return false;
+        return isSameTree(p->left,q->left) and isSameTree(p->right, q->right);
     }
 };
